ed2621d504f53c90d4820f91e04b834fe6579091
Emma <seebynum@gmail.com>
2024-08-20 14:20:34 +0800

    1

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -7,7 +7,7 @@ import { connectToDatabase } from "../database/mongoose";
 import { handleError } from "../utils";
 
 // CREATE
-export async function createUser(user: CreateUser Params) {
+export async function createUser(user: CreateUserParams) {
   try {
     await connectToDatabase();
 
